e462de8ce8afd438f0b9ee3ba9f4a7642cc5a523
Jake <sorme@kth.se>
2019-04-30 09:44:16 +0200

    Remove unused stuffs in ThreadPoolExecutor

--- chatserver/src/env/java/util/concurrent/ThreadPoolExecutor.java
+++ chatserver/src/env/java/util/concurrent/ThreadPoolExecutor.java
@@ -12,54 +12,23 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadPoolExecutor {
     private volatile int launchedThreads;
-    //private volatile int corePoolSize;
     private volatile int maximumPoolSize;
-    //private final BlockingQueue<Runnable> workQueue;
-    //private volatile long keepAliveTime;
-    //private volatile ThreadFactory threadFactory;
     private volatile RejectedExecutionHandler handler;
     public ThreadPoolExecutor(int corePoolSize, int maximumPoolSize,
 			      long keepAliveTime, TimeUnit unit,
 			      BlockingQueue<Runnable> workQueue,
 			      RejectedExecutionHandler handler) {
-        /*if (corePoolSize < 0 ||
-                maximumPoolSize <= 0 ||
-                maximumPoolSize < corePoolSize ||
-                keepAliveTime < 0)
-            throw new IllegalArgumentException();*/
-        if (maximumPoolSize <=0) throw  new IllegalArgumentException();
-        /*if (workQueue == null || threadFactory == null || handler == null)
-            throw new NullPointerException();*/
-        if(handler == null){
-            System.out.println("Handler in constructor");
-            throw new NullPointerException("handler");}
 
         this.maximumPoolSize = maximumPoolSize;
         this.handler = handler;
-        //this.corePoolSize = corePoolSize;
-        //this.workQueue = workQueue;
-        //this.keepAliveTime = unit.toNanos(keepAliveTime);
-        //this.threadFactory = threadFactory;
-
     }
 
     public void shutdown() { } // stub
 
     public void execute(Runnable r) {
-        //Keep track of number of threads launched
-            //Nr active threads: use the nr of launched threads as approximation (upper bound)
-        //Launch thread if not max number of threads reached
         if(launchedThreads<maximumPoolSize){
             r.run();
             launchedThreads++;
-        }else{
-            System.out.println("runnable " + r);
-            System.out.println("executor " + this);
-            System.out.println("handler " + handler);
-            handler.rejectedExecution(r, this);
-        }
-        //Reject thread if max nr reached by calling RejectedExecutionHandler
-            //Rejected case handled directly, do not launch new thread for this.
+        } else handler.rejectedExecution(r, this);
     }
-
 }
